6c5199a116796af4c0600c906d887cfff682150f
Amanatsu90 <s90.amano.natsuki@gmail.com>
2021-01-18 11:27:40 +0900

    Userモデルのテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   validates :name,     presence: true
   validates :nickname, presence: true, length: {maximum: 6}
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角英字と半角数字の両方を含めて設定してください'
 end
