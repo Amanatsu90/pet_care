4c08df854a722a96b8dfcd944b74c72a702eb695
Amanatsu90 <s90.amano.natsuki@gmail.com>
2021-01-19 10:31:56 +0900

    Active Storageの導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :text, :dog_size_id, :theme_id).merge(user_id: current_user.id)
+    params.require(:post).permit(:title, :text, :dog_size_id, :theme_id, :image).merge(user_id: current_user.id)
   end
 
 end
