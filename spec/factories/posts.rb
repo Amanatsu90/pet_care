6f54a2bb50500d8e9bb27e964d1c9407545cbdbc
Amanatsu90 <s90.amano.natsuki@gmail.com>
2021-02-08 21:14:51 +0900

    Postモデルのテスト

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,4 +1,13 @@
 FactoryBot.define do
   factory :post do
+    title       { Faker::Lorem.sentence(word_count: 3) }
+    text        { Faker::Lorem.paragraph(sentence_count: 2) }
+    dog_size_id { Faker::Number.between(from: 1, to: 3) }
+    theme_id    { Faker::Number.between(from: 1, to: 11) }
+    association :user
+
+    after(:build) do |post|
+      post.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @post = FactoryBot.build(:post)
+  end
+
+  describe '記事の投稿' do
+    it '必須項目が全て存在すれば保存可' do
+      expect(@post).to be_valid
+    end
+
+    it '画像が無くても保存可' do
+      @post.image = nil
+      expect(@post).to be_valid
+    end
+
+    it '記事のタイトルが空では保存不可' do
+      @post.title = ''
+      @post.valid?
+      expect(@post.errors.full_messages).to include('タイトルを入力してください')
+    end
+    
+    it '記事のテーマを選択しないと保存不可' do
+      @post.theme_id = ''
+      @post.valid?
+      expect(@post.errors.full_messages).to include('テーマを選択してください')
+    end
+    
+    it '愛犬のサイズを選択しないと保存不可' do
+      @post.dog_size_id = ''
+      @post.valid?
+      expect(@post.errors.full_messages).to include('愛犬のサイズを入力してください')
+    end
+    
+    it '本文が空では保存不可' do
+      @post.text = ''
+      @post.valid?
+      expect(@post.errors.full_messages).to include('本文を入力してください')
+    end
+  end
 end
