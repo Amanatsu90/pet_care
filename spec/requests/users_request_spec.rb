da2e671612e6c8ae0a46f35d5e2be561fb0df989
Amanatsu90 <s90.amano.natsuki@gmail.com>
2021-02-11 22:47:25 +0900

    ユーザーコントローラーのテスト

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,4 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+
+  describe 'GET #show' do
+    it '正常にレスポンスが返ってくる' do
+      get user_path(@user)
+      expect(response.status).to eq 200
+    end
+
+    it 'レスポンスに該当ユーザーが投稿した記事一覧が存在する' do
+      get user_path(@user)
+      expect(response.body).to include("#{@user.nickname}さんの投稿一覧")
+    end
+  end
+
+  describe 'GET #edit' do
+    it '正常にレスポンスが返ってくる' do
+      sign_in @user
+      get edit_user_path(@user)
+      expect(response.status).to eq 200
+    end
+
+    it 'レスポンスにユーザー名が存在する' do
+      sign_in @user
+      get edit_user_path(@user)
+      expect(response.body).to include(@user.name)
+    end
+
+    it 'レスポンスにユーザーのメールアドレスが存在する' do
+      sign_in @user
+      get edit_user_path(@user)
+      expect(response.body).to include(@user.email)
+    end
+  end
 end
